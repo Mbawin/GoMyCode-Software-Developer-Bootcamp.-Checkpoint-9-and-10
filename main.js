142bacb5c5f603d59ee0058b7042d61959848f05
Mbawin <gregorylamusah@gmail.com>
2024-07-27 10:38:29 +0000

    javascript checkpoint

---
+++ main.js
@@ -0,0 +1,7 @@
+let fName = "Greg"
+
+let country = "Ghana"
+
+let food = "jollof"
+
+console.log("My name is " + fName + ". I am from " + country + ". I love " + food + ".")
---
+++ practice1.js
@@ -0,0 +1,66 @@
+// let firstName = "Gregory"
+
+// let Age = 33
+
+// let hairColor = "black"
+
+// let numArray = [1, 2, 3, 4, 5, 6]
+
+// console.log("First Name is " + firstName)
+// console.log("Age is " + Age)
+// console.log("Hair Colour is " + hairColor)
+// console.log(numArray)
+
+// function addNumbers(a, b) 
+// {
+//     const sum = a + b;
+//     return sum;
+// }
+// console.log(addNumbers(5,4))
+
+
+// let tomorrow = "weekday"
+
+// if (tomorrow === "weekday") {
+//     console.log("it is a weekday!")}
+
+// else{
+//     console.log("Yaay, it is the weekend");
+// }
+
+
+// let fruit = "mango"
+
+// if (fruit === "mango") {
+//     console.log("it is a fruit!")}
+// else{
+//     console.log("it is not a fruit");
+// }
+
+
+// function addNumbers(a, b, c) 
+// {
+//     return a + b + c;
+// }
+// console.log(addNumbers(3, 8, 5))
+
+
+let fName = "Greg"
+
+switch (fName) {
+    case "Charles":
+        console.log("Hi Charles");
+        break;
+
+    case "James":
+        console.log("Yo James");
+        break;
+
+    case "Michael":
+        console.log("Hello My Gee");
+        break;       
+
+    default:
+        console.log("We do not know you");
+        break;
+}
